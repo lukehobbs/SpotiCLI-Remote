902c00be90d45467366152fdd9f36d7581919f9b
lukehobbs <lukeehobbs@gmail.com>
2017-04-06 23:24:02 -0400

    adding play and pause functionality

--- spotifycnct.go
+++ spotifycnct.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/user"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 
@@ -23,7 +24,12 @@ const tokendir = "/.spotifycnct"
 const tokenfile = tokendir + "/token.gob"
 
 var (
-	auth  = spotify.NewAuthenticator(redirectURL, spotify.ScopeUserReadPrivate, spotify.ScopeUserReadPlaybackState)
+	auth = spotify.NewAuthenticator(
+		redirectURL,
+		spotify.ScopeUserReadPrivate,
+		spotify.ScopeUserReadPlaybackState,
+		spotify.ScopeUserModifyPlaybackState,
+	)
 	state = "spotifycnct"
 	ch    = make(chan *spotify.Client)
 	tok   *oauth2.Token
@@ -84,7 +90,7 @@ func main() {
 		},
 	}
 	app.Usage = "Control Spotify Connect enabled devices via terminal."
-	app.UsageText = "command [command options] [arguments...]"
+	app.UsageText = "spotify> command [arguments...]"
 
 	// app.Flags = []cli.Flag{
 	// 	cli.StringFlag{
@@ -97,9 +103,45 @@ func main() {
 		{
 			Name:    "devices",
 			Aliases: []string{"d"},
-			Usage:   "List available devices",
+			Usage:   "List available devices and their IDs",
 			Action: func(c *cli.Context) error {
 				listDevices()
+				return nil
+			},
+		},
+		{
+			Name:    "play",
+      HelpName: "play DEVICE_ID, play DEVICE_ID",
+			Aliases: []string{"p"},
+			Usage:   "Start/Resume playback on `DEVICE_ID`, or currently playing device if not specified",
+			Action: func(c *cli.Context) error {
+				if c.Args().Get(0) != "" {
+					arg1, err := strconv.ParseUint(c.Args().Get(0), 10, 0)
+					if err != nil {
+						fmt.Println("Play command only accepts numbers [1, 2, 3...]")
+					} else {
+						play(arg1)
+					}
+					return nil
+				}
+				play(0)
+				return nil
+			},
+		},
+		{
+			Name:    "pause",
+			Aliases: []string{"pp"},
+			Usage:   "Pause playback on currently playing device",
+			Action: func(c *cli.Context) error {
+				pause()
+				return nil
+			},
+		},
+		{
+			Name:    "volume",
+			Aliases: []string{"v"},
+			Usage:   "Set volume on currently playing device to `PERCENT`",
+			Action: func(c *cli.Context) error {
 				return nil
 			},
 		},
@@ -131,6 +173,34 @@ func main() {
 	}
 
 	app.Run(os.Args)
+}
+
+func play(i uint64) {
+	client := auth.NewClient(tok)
+	if i == 0 {
+		err := client.Play()
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
+	devices, err := client.PlayerDevices()
+	if err != nil {
+		panic(err)
+	}
+	ID := []spotify.ID{devices[i-1].ID}
+	err = client.TransferPlayback(ID, true)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func pause() {
+	client := auth.NewClient(tok)
+	err := client.Pause()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func listDevices() {
